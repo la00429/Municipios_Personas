a631c803e159b08f7111505b1d1303229dd93cd0
Laura Figueredo <laura.figueredo02@uptc.edu.co>
2024-04-17 21:37:00 -0500

    Atributos y constructor en Municipio

--- src/co/edu/uptc/model/Township.java
+++ src/co/edu/uptc/model/Township.java
@@ -1,4 +1,15 @@
 package co.edu.uptc.model;
 
+import co.edu.uptc.structures.AVLTree;
+
+import java.util.Comparator;
+
 public class Township {
+    private String name;
+    private AVLTree<Inhabitant> inhabitants;
+
+    public Township(String name) {
+        this.name = name;
+        this.inhabitants = new AVLTree<>(Comparator.comparing(Inhabitant::getName));
+    }
 }
